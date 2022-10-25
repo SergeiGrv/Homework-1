7cb48cecfe4a076db9c3ad8a5448092d029fca8d
Sergei Gridnev <sergeigr32@gmail.com>
2022-10-25 21:07:48 +0300

    Added Main class and a readme file

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{23, 50, 67});
+        System.out.println(salesManager.max());
+    }
+}
