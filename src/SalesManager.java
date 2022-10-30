051a92fd4adfbe61becf2a15e0bde1ecdd0c82e8
Sergei Gridnev <sergeigr32@gmail.com>
2022-10-30 15:56:40 +0300

    Changed SalesManager

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,34 +1,13 @@
 public class SalesManager {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManager(int[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    int average;
-
-    public int averageSales() {
-        int minNumber = sales[0];
-        int maxNumber = sales[0];
-        int sum = 0;
-        if (sales.length < 5) {
-            System.out.println("Недостаточно данных!");
-        }
-        for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < minNumber) {
-                minNumber = sales[i];
-            } else if (sales[i] > maxNumber) {
-                maxNumber = sales[i];
-            }
-            sum += sales[i];
-        }
-        sum = sum - (minNumber + maxNumber);
-        return average = sum / (sales.length - 2);
-    }
-
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
