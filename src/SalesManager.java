5c6f980f2677e236c92c6a58ef6feb16b669a120
Sergei Gridnev <sergeigr32@gmail.com>
2022-11-01 22:38:07 +0300

    11

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,34 +1,13 @@
 public class SalesManager {
-    protected long[] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    long average;
-
-    public long averageSales() {
-        long minNumber = sales[0];
-        long maxNumber = sales[0];
-        long sum = 0;
-        if (sales.length < 5) {
-            System.out.println("Недостаточно данных!");
-        }
-        for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < minNumber) {
-                minNumber = sales[i];
-            } else if (sales[i] > maxNumber) {
-                maxNumber = sales[i];
-            }
-            sum += sales[i];
-        }
-        sum = sum - (minNumber + maxNumber);
-        return average = sum / (sales.length - 2);
-    }
-
-    public long max() {
-        long max = -1;
-        for (long sale : sales) {
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
